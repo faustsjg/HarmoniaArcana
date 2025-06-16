d710a90874a02f2d4c5731e76cca6de7c5c2c0d8
faustsjg <faustsjg@gmail.com>
2025-06-17 00:00:26 +0200

    v3.8.1-StableInteractive

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,7 @@
 // FILE: assets/js/config.js
 
 // Versió actual de l'aplicació
-export const APP_VERSION = "v3.8.0-StableUI";
+export const APP_VERSION = "v3.8.1-StableInteractive";
 
 // Clau que farem servir per guardar i recuperar l'API Key del localStorage.
 export const API_KEY_STORAGE_ID = 'harmoniaArcana_huggingFaceApiKey';
