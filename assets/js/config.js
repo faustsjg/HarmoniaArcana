6f0adedef65d3c77b9b961a98fe3bda2d7edf2a9
faustsjg <faustsjg@gmail.com>
2025-06-16 23:09:43 +0200

    v3.6.1-UIFix

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,7 @@
 // FILE: assets/js/config.js
 
 // Versió actual de l'aplicació
-export const APP_VERSION = "v3.6-FeatureReintegration";
+export const APP_VERSION = "v3.6.1-UIFix";
 
 // Clau que farem servir per guardar i recuperar l'API Key del localStorage.
 export const API_KEY_STORAGE_ID = 'harmoniaArcana_huggingFaceApiKey';
