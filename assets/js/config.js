6d277fa151854a8ebfe457620dc1fce0123d6e42
faustsjg <faustsjg@gmail.com>
2025-06-16 23:47:07 +0200

    v3.7.3-ForceDisplay

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,7 @@
 // FILE: assets/js/config.js
 
 // Versió actual de l'aplicació
-export const APP_VERSION = "v3.7.2-DebugUI";
+export const APP_VERSION = "v3.7.3-ForceDisplay";
 
 // Clau que farem servir per guardar i recuperar l'API Key del localStorage.
 export const API_KEY_STORAGE_ID = 'harmoniaArcana_huggingFaceApiKey';
