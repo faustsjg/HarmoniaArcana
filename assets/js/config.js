e58b102ce7d7697cc4b3f1164a67a787a34105e9
faustsjg <faustsjg@gmail.com>
2025-06-23 19:33:43 +0200

    v5.5.1-FullImplementation

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
 // FILE: assets/js/config.js
-export const APP_VERSION = "v5.5.0-StandbyAndLog";
+export const APP_VERSION = "v5.5.1-FullImplementation";
 export const API_KEY_STORAGE_ID = 'harmoniaArcana_huggingFaceApiKey';
 export const MODELS = {
     analyst: "meta-llama/Llama-3.1-8B-Instruct",
