7d9c248facc563fa9a025bca4783858d5d12d2d3
faustsjg <faustsjg@gmail.com>
2025-06-25 23:06:38 +0200

    v8.0.0-BardicUI-Complete

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,8 +1,9 @@
-export const APP_VERSION = "v7.0.0-GoldenMaster";
+// FILE: assets/js/config.js
+export const APP_VERSION = "v8.0.0-BardicUI-Complete";
 export const API_KEY_STORAGE_ID = 'harmoniaArcana_huggingFaceApiKey';
 export const MODELS = {
     analyst: "meta-llama/Llama-3.1-8B-Instruct",
-    musicgen: "facebook/musicgen-melody"
+    musicgen: "facebook/musicgen-melody" 
 };
 export const DIRECTOR_CONFIG = {
     analysisInterval: 20000,
