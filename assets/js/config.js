15d5bf326b45dd9f656d506fd4b56d9503b82b62
faustsjg <faustsjg@gmail.com>
2025-06-25 22:31:17 +0200

    v7.0.0-GoldenMaster

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,9 +1,8 @@
-// FILE: assets/js/config.js
 export const APP_VERSION = "v7.0.0-GoldenMaster";
 export const API_KEY_STORAGE_ID = 'harmoniaArcana_huggingFaceApiKey';
 export const MODELS = {
     analyst: "meta-llama/Llama-3.1-8B-Instruct",
-    musicgen: "facebook/musicgen-melody" 
+    musicgen: "facebook/musicgen-melody"
 };
 export const DIRECTOR_CONFIG = {
     analysisInterval: 20000,
