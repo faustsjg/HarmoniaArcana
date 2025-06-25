5aed478c2ac6c292b41ab7181f0049fdaea08098
faustsjg <faustsjg@gmail.com>
2025-06-25 18:08:30 +0200

    v6.0.1-FinalFix

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,11 +1,11 @@
-export const APP_VERSION = "v6.0.0-DefinitiveReset";
+// FILE: assets/js/config.js
+export const APP_VERSION = "v6.0.1-FinalFix";
 export const API_KEY_STORAGE_ID = 'harmoniaArcana_huggingFaceApiKey';
 export const MODELS = {
     analyst: "meta-llama/Llama-3.1-8B-Instruct",
-    musicgen: "facebook/musicgen-small" 
+    musicgen: "facebook/musicgen-melody" 
 };
 export const DIRECTOR_CONFIG = {
     analysisInterval: 20000,
     minCharsForAnalysis: 30
 };
-
