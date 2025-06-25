16acb6af543040b169988e50b450d87d26c56108
faustsjg <faustsjg@gmail.com>
2025-06-25 18:09:29 +0200

    v6.0.1-MusicEngineFix

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,8 +1,9 @@
 // FILE: assets/js/config.js
-export const APP_VERSION = "v6.0.1-FinalFix";
+export const APP_VERSION = "v6.0.1-MusicEngineFix";
 export const API_KEY_STORAGE_ID = 'harmoniaArcana_huggingFaceApiKey';
 export const MODELS = {
     analyst: "meta-llama/Llama-3.1-8B-Instruct",
+    // CANVI: Utilitzem un model alternatiu i molt fiable de la mateixa família.
     musicgen: "facebook/musicgen-melody" 
 };
 export const DIRECTOR_CONFIG = {
