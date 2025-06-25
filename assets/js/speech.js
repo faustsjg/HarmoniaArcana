7f3c32ac31fa201a67b5719a40c2903e04738086
faustsjg <faustsjg@gmail.com>
2025-06-25 22:37:12 +0200

    v7.0.0-GoldenMaster

--- assets/js/speech.js
+++ assets/js/speech.js
@@ -1,57 +1,38 @@
 // FILE: assets/js/speech.js
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-
 export const Speech = {
-    recognition: null,
-    transcriptBuffer: "",
-    isListening: false,
-    
+    recognition: null, transcriptBuffer: "", isListening: false,
     init(onResult, onFinalResult) {
-        if (!SpeechRecognition) return false;
-        
+        if (!SpeechRecognition) { alert("El reconeixement de veu no és compatible."); return false; }
         this.recognition = new SpeechRecognition();
         this.recognition.lang = 'ca-ES';
-        this.recognition.interimResults = true; // <-- CANVI CLAU
+        this.recognition.interimResults = true;
         this.recognition.continuous = true;
-
         this.recognition.onresult = (event) => {
-            let interim_transcript = '';
-            let final_transcript = '';
-
+            let interim_transcript = ''; let final_transcript = '';
             for (let i = event.resultIndex; i < event.results.length; ++i) {
-                if (event.results[i].isFinal) {
-                    final_transcript += event.results[i][0].transcript;
-                } else {
-                    interim_transcript += event.results[i][0].transcript;
-                }
+                if (event.results[i].isFinal) { final_transcript += event.results[i][0].transcript; } 
+                else { interim_transcript += event.results[i][0].transcript; }
             }
-            
-            // Enviem el text final acumulat per a l'anàlisi de la IA
-            if (final_transcript) {
-                this.transcriptBuffer += final_transcript;
-                onFinalResult(final_transcript);
-            }
-            // I enviem el text provisional per a una visualització ràpida
-            if (onResult) {
-                onResult(interim_transcript);
-            }
+            if (final_transcript) { this.transcriptBuffer += final_transcript + " "; onFinalResult(final_transcript + " "); }
+            if (onResult) { onResult(interim_transcript); }
         };
-
-        this.recognition.onend = () => {
-            if (this.isListening) this.recognition.start();
-        };
+        this.recognition.onend = () => { if (this.isListening) this.recognition.start(); };
         return true;
     },
-
     startListening() {
         if (this.recognition && !this.isListening) {
-            this.transcriptBuffer = "";
-            this.isListening = true;
-            this.recognition.start();
-            console.log("Speech: Escoltant...");
+            this.transcriptBuffer = ""; this.isListening = true; this.recognition.start();
         }
     },
-    // ... la resta de funcions (stopListening, getAndClearBuffer) es mantenen igual ...
-    stopListening() { /*...*/ },
-    getAndClearBuffer() { /*...*/ }
+    stopListening() {
+        if (this.recognition && this.isListening) {
+            this.isListening = false; this.recognition.stop();
+        }
+    },
+    getAndClearBuffer() {
+        const buffer = this.transcriptBuffer || "";
+        this.transcriptBuffer = "";
+        return buffer;
+    }
 };
