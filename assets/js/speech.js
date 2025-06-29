046036be9b0c60dc2a00788518f766069236fdf3
faustsjg <faustsjg@gmail.com>
2025-06-30 01:50:46 +0200

    v10.0.0-IntelligentDJ-Final

--- assets/js/speech.js
+++ assets/js/speech.js
@@ -1,24 +1,42 @@
 // FILE: assets/js/speech.js
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 export const Speech = {
-    recognition: null, transcriptBuffer: "", isListening: false,
-    init(onResult, onFinalResult) {
+    recognition: null,
+    transcriptBuffer: "",
+    isListening: false,
+    init(onResult) {
         if (!SpeechRecognition) { alert("El reconeixement de veu no és compatible."); return false; }
         this.recognition = new SpeechRecognition();
-        this.recognition.lang = 'ca-ES'; this.recognition.interimResults = true; this.recognition.continuous = true;
+        this.recognition.lang = 'ca-ES';
+        this.recognition.interimResults = true;
+        this.recognition.continuous = true;
         this.recognition.onresult = (event) => {
-            let interim_transcript = ''; let final_transcript = '';
+            let newText = "";
             for (let i = event.resultIndex; i < event.results.length; ++i) {
-                if (event.results[i].isFinal) { final_transcript += event.results[i][0].transcript; } 
-                else { interim_transcript += event.results[i][0].transcript; }
+                newText += event.results[i][0].transcript;
             }
-            if (final_transcript) { this.transcriptBuffer += final_transcript + " "; onFinalResult(final_transcript + " "); }
-            if (onResult) { onResult(interim_transcript); }
+            this.transcriptBuffer += newText;
+            onResult(this.transcriptBuffer);
         };
         this.recognition.onend = () => { if (this.isListening) this.recognition.start(); };
         return true;
     },
-    startListening() { if (this.recognition && !this.isListening) { this.transcriptBuffer = ""; this.isListening = true; this.recognition.start(); }},
-    stopListening() { if (this.recognition && this.isListening) { this.isListening = false; this.recognition.stop(); }},
-    getAndClearBuffer() { const buffer = this.transcriptBuffer || ""; this.transcriptBuffer = ""; return buffer; }
+    startListening() {
+        if (this.recognition && !this.isListening) {
+            this.transcriptBuffer = "";
+            this.isListening = true;
+            this.recognition.start();
+        }
+    },
+    stopListening() {
+        if (this.recognition && this.isListening) {
+            this.isListening = false;
+            this.recognition.stop();
+        }
+    },
+    getAndClearBuffer() {
+        const buffer = this.transcriptBuffer || "";
+        this.transcriptBuffer = "";
+        return buffer;
+    }
 };
