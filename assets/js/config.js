f6e107ec7fd4ae83e8d0f486550b9c5e667534e3
faustsjg <faustsjg@gmail.com>
2025-06-30 18:49:56 +0200

    v12.0.0-PhoenixRise

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,6 @@
-export const APP_VERSION = "v10.0.0-IntelligentDJ-Final";
+export const APP_VERSION = "v12.0.0-PhoenixRise";
 export const API_KEY_STORAGE_ID = 'harmoniaArcana_huggingFaceApiKey';
 export const MODELS = {
-    // Només necessitem l'analista de text per determinar el 'mood'
     analyst: "meta-llama/Llama-3.1-8B-Instruct"
 };
 export const DIRECTOR_CONFIG = {
