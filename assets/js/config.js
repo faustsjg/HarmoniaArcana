ce9d1ad8c6a1b8207e823dafb0d710e8bcf15826
faustsjg <faustsjg@gmail.com>
2025-07-01 10:09:39 +0200

    v12.2.0-FullUIRestore

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,6 @@
-export const APP_VERSION = "v12.1.0-FinalUI";
+export const APP_VERSION = "v12.2.0-FullUIRestore";
 export const API_KEY_STORAGE_ID = 'harmoniaArcana_huggingFaceApiKey';
+// Deixem l'analista de text, que és l'única IA que farem servir en aquesta arquitectura
 export const MODELS = {
     analyst: "meta-llama/Llama-3.1-8B-Instruct"
 };
