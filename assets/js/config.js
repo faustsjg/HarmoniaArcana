194316727caf21fd55c01eb5aaca024be672c447
faustsjg <faustsjg@gmail.com>
2025-07-01 09:56:28 +0200

    v12.1.0-FinalUI

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,4 @@
-export const APP_VERSION = "v12.0.0-PhoenixRise";
+export const APP_VERSION = "v12.1.0-FinalUI";
 export const API_KEY_STORAGE_ID = 'harmoniaArcana_huggingFaceApiKey';
 export const MODELS = {
     analyst: "meta-llama/Llama-3.1-8B-Instruct"
