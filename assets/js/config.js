88b2485d1016c80ff5d130c88fe0435f749bef9f
faustsjg <faustsjg@gmail.com>
2025-07-01 11:26:27 +0200

    v12.3.0-FinalFlow

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,5 @@
-export const APP_VERSION = "v12.2.0-FullUIRestore";
+export const APP_VERSION = "v12.3.0-FinalFlow";
 export const API_KEY_STORAGE_ID = 'harmoniaArcana_huggingFaceApiKey';
-// Deixem l'analista de text, que és l'única IA que farem servir en aquesta arquitectura
 export const MODELS = {
     analyst: "meta-llama/Llama-3.1-8B-Instruct"
 };
