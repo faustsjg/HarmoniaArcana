e0f71437b642fff1dff207d6f83c03c0d90ebf4b
faustsjg <faustsjg@gmail.com>
2025-07-03 08:38:25 +0200

    v13.2.0

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,10 +1,8 @@
-// FILE: assets/js/config.js
-export const APP_VERSION = "v13.1.1";
+export const APP_VERSION = "v13.2.0";
 
 export const API_KEY_STORAGE_ID = 'harmoniaArcana_huggingFaceApiKey';
 
 export const MODELS = {
-  // Només necessitem l'analista de text per determinar el 'mood'
   analyst: "meta-llama/Llama-3.1-8B-Instruct"
 };
 
