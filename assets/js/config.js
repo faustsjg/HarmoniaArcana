9ae05840e58f9d1ce1b3f86ca32833c6e1517361
faustsjg <faustsjg@gmail.com>
2025-07-04 10:06:25 +0200

    v13.3.0

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,4 @@
-export const APP_VERSION = "v13.2.2";
+export const APP_VERSION = "v13.3.0";
 export const API_KEY_STORAGE_ID = 'harmoniaArcana_huggingFaceApiKey';
 export const MODELS = {
   analyst: "meta-llama/Llama-3.1-8B-Instruct"
