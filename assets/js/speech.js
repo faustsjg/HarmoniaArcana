fff12cd488cc915444be5d0479b7e0b40f6265ea
faustsjg <faustsjg@gmail.com>
2025-07-05 23:24:25 +0200

    v13.3.7

--- assets/js/speech.js
+++ assets/js/speech.js
@@ -1,35 +1,48 @@
-const SR = window.SpeechRecognition || window.webkitSpeechRecognition;
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
 export const Speech = {
-  recognition: null, transcriptBuffer:'', isListening:false,
-  init(cb) {
-    if (!SR) { alert("Reconeixement no suportat"); return false; }
-    this.recognition = new SR();
+  recognition: null,
+  transcript: '',
+  isListening: false,
+
+  init(callback) {
+    if (!SpeechRecognition) return alert('No compatible');
+    this.recognition = new SpeechRecognition();
     this.recognition.lang = 'ca-ES';
-    this.recognition.interimResults = true;
     this.recognition.continuous = true;
+    this.recognition.interimResults = true;
+
     this.recognition.onresult = e => {
-      let t=''; for (let i=e.resultIndex;i<e.results.length;++i) t += e.results[i][0].transcript;
-      this.transcriptBuffer += t;
-      cb(this.transcriptBuffer);
+      let text = '';
+      for (let i = e.resultIndex; i < e.results.length; i++) {
+        text += e.results[i][0].transcript;
+      }
+      this.transcript += text;
+      callback(this.transcript);
     };
-    this.recognition.onend = () => this.isListening && this.recognition.start();
-    return true;
+    this.recognition.onend = () => {
+      if (this.isListening) this.recognition.start();
+    };
   },
+
   startListening() {
     if (this.recognition && !this.isListening) {
-      this.transcriptBuffer=''; this.isListening=true;
+      this.transcript = '';
+      this.isListening = true;
       this.recognition.start();
     }
   },
+
   stopListening() {
-    if (this.recognition&&this.isListening) {
-      this.isListening=false;
+    if (this.recognition && this.isListening) {
+      this.isListening = false;
       this.recognition.stop();
     }
   },
+
   getAndClearBuffer() {
-    const b=this.transcriptBuffer;
-    this.transcriptBuffer='';
-    return b;
+    const text = this.transcript;
+    this.transcript = '';
+    return text;
   }
 };
