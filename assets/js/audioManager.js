11b46a251dae405aefc05eaf52f5d220f3510261
faustsjg <faustsjg@gmail.com>
2025-07-06 17:01:38 +0200

    v13.4.1

--- assets/js/audioManager.js
+++ assets/js/audioManager.js
@@ -1,32 +1,71 @@
 export const AudioManager = {
-  isInit: false, player: null, effect: null,
+  isInitialized: false,
+  player: null,
+  effect: null,
+
   async init() {
-    if (this.isInit) return;
+    if (this.isInitialized) return;
     await Tone.start();
     Tone.Transport.start();
-    this.isInit = true;
+    this.isInitialized = true;
   },
-  playTrack(url, moodName) {
+
+  playTrack(url, moodName = '') {
     if (this.player) {
-      this.player.stop(); this.player.dispose();
+      // fade out
+      this.player.volume.rampTo(-Infinity, 1);
+      // stop/despose old after fade
+      setTimeout(() => {
+        this.player.stop();
+        this.player.dispose();
+      }, 1000);
     }
-    this.player = new Tone.Player({url, loop:true, volume:-Infinity, autostart:true})
-      .toDestination();
-    this.player.volume.rampTo(0, 2);
+
+    this.player = new Tone.Player({
+      url,
+      loop: true,
+      volume: -Infinity,
+      autostart: true,
+      onload: () => {
+        this.player.volume.rampTo(0, 2);
+      },
+      onerror: err => console.error('Error track:', err)
+    }).toDestination();
   },
+
   toggleTrack() {
     if (!this.player) return;
-    this.player.state === 'started' ? this.player.stop() : this.player.start();
-  },
-  stopTrack() {
-    if (this.player) {
-      this.player.stop(); this.player.dispose();
-      this.player = null;
+    if (this.player.state === 'started') {
+      this.player.volume.rampTo(-Infinity, 1);
+      setTimeout(() => this.player.stop(), 1000);
+    } else {
+      this.player.start();
+      this.player.volume.rampTo(0, 1);
     }
   },
+
+  stopTrack() {
+    if (this.player) {
+      this.player.volume.rampTo(-Infinity, 1);
+      setTimeout(() => {
+        this.player.stop();
+        this.player.dispose();
+        this.player = null;
+      }, 1000);
+    }
+  },
+
   playEffect(id) {
     const url = `assets/sounds/${id}.mp3`;
-    if (this.effect) { this.effect.stop(); this.effect.dispose(); }
-    this.effect = new Tone.Player({url, volume:-6, autostart:true}).toDestination();
+    if (this.effect) {
+      this.effect.stop();
+      this.effect.dispose();
+    }
+    this.effect = new Tone.Player({
+      url,
+      volume: -6,
+      autostart: true,
+      onerror: err => console.error('Error effect:', err)
+    }).toDestination();
   }
 };
